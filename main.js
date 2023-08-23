a90c555c88c6b99e866efd8877e829926c6bd8bc
Riaz Anwar <iamriazanwar@gmail.com>
2023-08-23 19:25:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+console.log("Riaz Anwar");
+console.log("01/01/1998");
+console.log("(01/01/1998)");
+console.log("Riaz Anwar (1998)"); 
+console.log("Riaz", "Anwar", "(1998)");
+console.log("Riaz"+ " " + "Anwar")
+
